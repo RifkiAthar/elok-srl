d8c4dee471eb40d3c21717f0e90db8997effc2ee
Rifki Athar <37376293+RifkiAthar@users.noreply.github.com>
2021-06-09 14:19:16 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,38 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'us-cdbr-east-04.cleardb.com';
+$CFG->dbname    = 'heroku_85fbe23ad02fdc2';
+$CFG->dbuser    = 'b30ac77ad44c0c';
+$CFG->dbpass    = 'd5701e4c';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+$CFG->wwwroot   = 'http://elok-srl.herokuapp.com';
+$CFG->dataroot  = 'moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+$CFG->debug = E_ALL;
+$CFG->debugdisplay = 1;
+$CFG->langstringcache = 0;
+$CFG->cachetemplates = 0;
+$CFG->cachejs = 0;
+$CFG->perfdebug = 15;
+$CFG->debugpageinfo = 1;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
